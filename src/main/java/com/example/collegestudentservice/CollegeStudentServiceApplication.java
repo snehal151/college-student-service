77887169b550b8779df88af350bbfea9f89051b4
Sneha <snehal.gavali27@gmail.com>
2020-03-26 15:13:19 +0000

    New Service added

---
+++ src/main/java/com/example/collegestudentservice/CollegeStudentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.collegestudentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeStudentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeStudentServiceApplication.class, args);
+	}
+
+}
